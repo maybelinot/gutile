c8e41fbdc04fdcf36358cd402fd8ff302c3be6c3
etrott <etrott@redhat.com>
2015-11-26 15:05:05 +0100

    Increase version for Pypi

--- gutile/_version.py
+++ gutile/_version.py
@@ -4,10 +4,10 @@
 # @Date:   2015-09-15 08:57:35
 # @Email:  etrott@redhat.com
 # @Last modified by:   etrott
-# @Last Modified time: 2015-10-08 16:11:21
+# @Last Modified time: 2015-11-26 15:04:16
 
 
 from __future__ import unicode_literals, absolute_import
 
-version_info = ('0', '0', '2')
+version_info = ('0', '0', '3')
 __version__ = '.'.join(version_info[0:3])  # + '-' + version_info[3]
